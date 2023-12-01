32e03650bebfc8d814bf600e535afa76882833f3
Felician <schmalzahar@web.de>
2023-12-01 16:40:07 +0100

    Cleanup day 1

--- 2023/a01.m
+++ 2023/a01.m
@@ -1,12 +1,13 @@
 input = readlines("a01.txt");
-first = regexp(input,'\d','match','once');
-last = regexp(input,'\d(?=\D*$)','match','once');
-sum(str2double(first+last))
+count(input)
 wordList = {'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'};
 numberList = {"o1e", "t2o", "t3e", "4", "5e", "6", "7n", "e8t", "9e"};
 for i=1:9                                       
     input = regexprep(input,wordList{i}, numberList{i});
 end
-first = regexp(input,'\d','match','once');
-last = regexp(input,'\d(?=\D*$)','match','once');
-sum(str2double(first+last))
+count(input)
+function out = count(input)
+    first = regexp(input,'\d','match','once');
+    last = regexp(input,'\d(?=\D*$)','match','once');
+    out = sum(str2double(first+last));
+end
