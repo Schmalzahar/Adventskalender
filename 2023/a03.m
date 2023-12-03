126ea98140996e67561ac53a641e760281b36c52
Felician <schmalzahar@web.de>
2023-12-03 18:30:04 +0100

    nice version

--- 2023/a03.m
+++ 2023/a03.m
@@ -1,44 +1,24 @@
 input = char(readlines("a03.txt"));
-chars = [];
-parts = struct;
-for i = 1:numel(input)
-    if ~contains('01234566789.', input(i))
-        chars(end + 1,1) = i;
-    end
-end
+chars = find(arrayfun(@(x) ~contains('01234566789.', x), input));
+parts = dictionary;
 for r = 1:size(input,1)
     row = input(r,:);
     matches = regexp(row, '(\d+)', 'tokenExtents');
     for m = matches
-        nexts = [];
-        ma = m{:};
-        for s=-1:1
-            for d=-1:1
-                for c = ma(1):ma(end)
-                    if (r+s>0) && (r+s<=size(input,1))...
-                            && (c+d>0) && (c+d<=size(input,2))
-                        nexts(end + 1,:) = sub2ind(size(input),r+s,c+d);
-                    end
-                end
-            end
-        end      
-        % Filter neighbors that are also in chars
-        nexts = intersect(nexts, chars, 'rows');    
+        ma = m{:};               
+        rows = max(1, r - 1):min(size(input, 1), r + 1);
+        cols = max(1, ma(1) - 1):min(size(input, 2), ma(2) + 1);
+        [row_indices, col_indices] = meshgrid(rows, cols);        
+        nexts = sub2ind(size(input),row_indices,col_indices);   
+        nexts = intersect(reshape(nexts,[],1), chars, 'rows');    
         if size(nexts,1)>0
-            if isfield(parts, ['A',num2str(nexts)])
-                parts.(['A',num2str(nexts)]) = [parts.(['A',num2str(nexts)]) str2double(row(ma(1):ma(end)))];
+            if ~isConfigured(parts) || ~isKey(parts,nexts)
+                parts(nexts) = {str2double(row(ma(1):ma(end)))};
             else
-                parts.(['A',num2str(nexts)]) = str2double(row(ma(1):ma(end)));
-            end                
+                parts(nexts) = {[parts{nexts}, str2double(row(ma(1):ma(end)))]};
+            end           
         end
     end
 end
-pv = fieldnames(parts);
-res1 = 0;
-res2 = 0;
-for p = pv'
-    res1 = res1 + sum(parts.(p{:}));
-    if size(parts.(p{:}),2) == 2
-        res2 = res2 + prod(parts.(p{:}));
-    end
-end
+res1 = sum([parts.values{:}],'all');
+res2 = sum(arrayfun(@(x) (size(parts.values{x},2)==2)*prod(parts.values{x}), 1:parts.numEntries));
