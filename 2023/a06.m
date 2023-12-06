8a1636d62c6509e7e1723afcf717fdb32270d2f8
Felician Putz <putz@fdy.tu-darmstadt.de>
2023-12-06 15:27:08 +0100

    day 6 with math

--- 2023/a06.m
+++ 2023/a06.m
@@ -1,15 +1,5 @@
 input = readlines("a06.txt").extract(digitsPattern).double;
-part2 = strrep(string(num2str(input))," ","").double;
-input = part2;
-winning = zeros(1,size(input,2));
-for i=1:size(input,2)
-    for j=0:input(1,i)
-        speed = j;
-        remaining_time = input(1,i)-j;
-        dist = speed * remaining_time;
-        if dist>input(2,i)
-            winning(i) = winning(i) + 1;
-        end
-    end
-end
-prod(winning)
+% input = strrep(string(num2str(input))," ","").double; % for part 2
+% with math: solve the quadratic equation x^2-xt+d<0, with t being the end
+% time and d the minimum distance. The result is floor(x1)-floor(x2).
+prod(floor(input(1,:)/2 + sqrt(input(1,:).^2-4*input(2,:))/2) - floor(input(1,:)/2 - sqrt(input(1,:).^2-4*input(2,:))/2))
