99726f7ff3fac698e6891b07075573e8bd6bd17f
Felician Putz <putz@fdy.tu-darmstadt.de>
2023-12-08 14:27:34 +0100

    with dicts, still slow

--- 2023/a08.m
+++ 2023/a08.m
@@ -1,36 +1,39 @@
 input = readlines("a08.txt");
 tic
 old_dir = char(input(1));
+% build dictionary from networks
 network = input(3:end);
 network =  regexp(network,'(\w\w\w)','match');
-network = table2array(cell2table(network));
-starts = char(network(:,1));
+network_t = table2array(cell2table(network));
+starts = char(network_t(:,1));
+netdict = dictionary(starts,network);
 % elements = 'AAA'; % part 1
 elements = starts(starts(:,3) == 'A',:); % part 2
-global_steps = zeros(size(elements,1),1);
-for i=1:size(elements,1)
-    steps = 1;
-    element = elements(i,:);
-    dir = old_dir;
-    while true
-        res = network(element == network(:,1),2:3);
-        if dir(1) == 'L'
-            element = char(res(:,1));
-        else
-            element = char(res(:,2));
-        end
-        if element(:,3) == 'Z'
-            global_steps(i) = steps;
-            break
-        end
-        dir = circshift(dir,-1);
-        steps = steps + 1;
+global_steps = [];
+steps = 1;
+while true
+    res = netdict(elements);
+    res = reshape([res{:}],3,[])';
+    if dir(1) == 'L'
+        elements = char(res(:,2));
+    else
+        elements = char(res(:,3));
+    end
+    if any(elements(:,3) == 'Z')
+        global_steps(end+1) = steps;  
+        elements(elements(:,3) == 'Z',:) = [];
+    end
+    dir = circshift(dir,-1);
+    steps = steps + 1;
+    if size(global_steps,2) == 6
+        break
     end
 end
+
 eval_str = '';
-for i=1:size(global_steps,1)-1
+for i=1:size(global_steps,2)-1
     eval_str = [eval_str,'lcm(',num2str(global_steps(i)),','];
 end
-eval_str = [eval_str,num2str(global_steps(end)),repelem(')',size(global_steps,1)-1)];
+eval_str = [eval_str,num2str(global_steps(end)),repelem(')',size(global_steps,2)-1)];
 eval(eval_str)
 toc
