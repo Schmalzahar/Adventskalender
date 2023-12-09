8f2014ec0082a946dd9f2e5acf2686ffd61ea48e
Felician <schmalzahar@web.de>
2023-12-09 09:19:03 +0100

    .

--- 2023/a09.m
+++ 2023/a09.m
@@ -3,15 +3,13 @@ new_end_val = [];
 new_first_val = [];
 for i=1:size(input,1)
     line = input(i).split(' ').double';
-    last_val = line(end);
-    first_val = line(1);
+    new_vals = [line(1) line(end)];
     while ~all(line == 0)
         line = diff(line);
-        last_val = [last_val; line(end)];
-        first_val = [first_val; line(1)];
+        new_vals = [new_vals; line(1) line(end)];
     end
-    new_end_val = [new_end_val ; sum(last_val)];
-    new_first_val = [new_first_val; sum((-1).^((1:length(first_val))+1).*first_val')];
+    new_end_val = [new_end_val ; sum(new_vals(:,2))];
+    new_first_val = [new_first_val; sum((-1).^((1:length(new_vals(:,1)))+1).*new_vals(:,1)')];
 end
 sum(new_end_val)
 sum(new_first_val)
