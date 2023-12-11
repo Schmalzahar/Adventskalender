8f38204eb6e95e057ca33a65e39a593bdfd0232f
Felician Putz <putz@fdy.tu-darmstadt.de>
2023-12-11 17:08:30 +0100

    7 Zeilen

--- 2023/a11.m
+++ 2023/a11.m
@@ -1,14 +1,7 @@
 galaxies = char(readlines('a11.txt')) == '#';
-expand_col = find(sum(galaxies) == 0);
-expand_row = find(sum(galaxies,2) == 0);
-expansion = 10^6-1; % part 1: 1, part 2: 10^6-1
-pairs = nchoosek(find(galaxies),2);
-[rs, cs] = ind2sub(size(galaxies), pairs);
-rs = sort(rs,2); cs = sort(cs,2);
-res = sum(abs(rs(:,1)-rs(:,2))+abs(cs(:,1)-cs(:,2)));
-for i=1:height(pairs)
-    row_dist = sum(rs(i,1)<expand_row & expand_row < rs(i,2));
-    col_dist = sum(cs(i,1)<expand_col & expand_col < cs(i,2));
-    res = res + (row_dist + col_dist) * expansion;
-end
-res
+[r,c] = find(galaxies);
+cs = cumsum(ones(1,size(galaxies,1)) + ~any(galaxies)*(10^6-1));
+rs = cumsum(ones(size(galaxies,2),1) + ~any(galaxies,2)*(10^6-1));
+r = rs(r);
+c = cs(c)';
+(sum(abs(c-c') + abs(r-r'),"all"))/2
