3070d77f0f342fe2c6adeb0694a93fa63ab5040f
Felician Putz <putz@fdy.tu-darmstadt.de>
2023-12-11 13:04:32 +0100

    part 1 and part 2

--- 2023/a11.m
+++ 2023/a11.m
@@ -1,15 +1,14 @@
-input = char(readlines('a11.txt'));
-galaxies = input == '#';
+galaxies = char(readlines('a11.txt')) == '#';
 expand_col = find(sum(galaxies) == 0);
 expand_row = find(sum(galaxies,2) == 0);
-for r = flip(expand_row)'
-    input = [input(1:r,:); repelem('.',size(input,2)); input(r+1:end,:)];
+expansion = 10^6-1; % part 1: 1, part 2: 10^6-1
+pairs = nchoosek(find(galaxies),2);
+[rs, cs] = ind2sub(size(galaxies), pairs);
+rs = sort(rs,2); cs = sort(cs,2);
+res = sum(abs(rs(:,1)-rs(:,2))+abs(cs(:,1)-cs(:,2)));
+for i=1:height(pairs)
+    row_dist = sum(rs(i,1)<expand_row & expand_row < rs(i,2));
+    col_dist = sum(cs(i,1)<expand_col & expand_col < cs(i,2));
+    res = res + (row_dist + col_dist) * expansion;
 end
-for c = flip(expand_col)
-    input = [input(:,1:c) repelem('.',size(input,1))' input(:,c+1:end)];
-end
-galaxies = find(input == '#');
-pairs = nchoosek(galaxies,2);
-paths = zeros(size(pairs,1),1);
-[rs, cs] = ind2sub(size(input), pairs);
-sum(abs(rs(:,1)-rs(:,2))+abs(cs(:,1)-cs(:,2)))
+res
