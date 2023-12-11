f5e6349b580ff3459e3b0974105854b2fdab04ef
Felician Putz <putz@fdy.tu-darmstadt.de>
2023-12-11 17:11:03 +0100

    5

--- 2023/a11.m
+++ 2023/a11.m
@@ -2,6 +2,4 @@ galaxies = char(readlines('a11.txt')) == '#';
 [r,c] = find(galaxies);
 cs = cumsum(ones(1,size(galaxies,1)) + ~any(galaxies)*(10^6-1));
 rs = cumsum(ones(size(galaxies,2),1) + ~any(galaxies,2)*(10^6-1));
-r = rs(r);
-c = cs(c)';
-(sum(abs(c-c') + abs(r-r'),"all"))/2
+(sum(abs(cs(c)-cs(c)') + abs(rs(r)-rs(r)'),"all"))/2
