a14f8dcf7c217dd54070af2c3514dfc77d03cf5b
Felician <schmalzahar@web.de>
2023-12-13 21:20:19 +0100

    fixed

--- 2023/a13.m
+++ 2023/a13.m
@@ -1,8 +1,8 @@
 input = [""; readlines('a13.txt');""];
 tic
 spl = find(input == "");
-res = NaN(100,1);
-res2 = NaN(100,1);
+res = 0;
+res2 = 0;
 multis = [1 100]; 
 for i=2:length(spl)    
     block = char(input(spl(i-1)+1:spl(i)-1));
@@ -15,7 +15,7 @@ for i=2:length(spl)
     equalRow1 = squeeze(sum(trow == block,2)>=size(block,2)-1);
     [bcol1,~] = find(conv2(equalCol1, ones(2), 'valid') == 2^2);
     [brow1,~] = find(conv2(equalRow1, ones(2), 'valid') == 2^2);
-    bdir1 = {bcol1, brow1};
+    bdir1 = {unique(bcol1), unique(brow1)};
     tdir = {equalCol, equalRow};    
     tdir1 = {equalCol1, equalRow1};
     for j=1:2
@@ -23,9 +23,9 @@ for i=2:length(spl)
             bd = bdir1{j}(k);
             sz = min(size(tdir1{j},1) - bd, bd);
             if all(diag(tdir{j}(bd-sz+1:bd+sz,bd-sz+1:bd+sz))) && all(diag(tdir{j}(bd-sz+1:bd+sz,bd+sz:-1:bd-sz+1)))
-                res(i-1) = multis(j) * bd;            
+                res = res +  multis(j) * bd;            
             elseif all(diag(tdir1{j}(bd-sz+1:bd+sz,bd-sz+1:bd+sz))) && all(diag(tdir1{j}(bd-sz+1:bd+sz,bd+sz:-1:bd-sz+1)))
-                res2(i-1) = multis(j) * bd;
+                res2 = res2 + multis(j) * bd;
             end
         end
     end
