28d1345e0bbdd20f36ebbf9ff05946b429a8fc42
Felician Putz <putz@fdy.tu-darmstadt.de>
2023-12-13 14:16:18 +0100

    part 1 correct

--- 2023/a13.m
+++ 2023/a13.m
@@ -1,6 +1,7 @@
 input = [""; readlines('a13.txt');""];
 spl = find(input == "");
 res = 0;
+multis = [1 100]; 
 tic
 for i=2:length(spl)
     block = char(input(spl(i-1)+1:spl(i)-1));
@@ -10,16 +11,14 @@ for i=2:length(spl)
     equalRow = squeeze(all(trow == block,2));
     bcol = find(all(equalCol(:,1:end-1) == equalCol(:,2:end),1));
     brow = find(all(equalRow(:,1:end-1) == equalRow(:,2:end),1));
-    for bc = bcol
-        sz = min(size(equalCol,1) - bc, bc);
-        if all(sum(equalCol(bc-sz+1:bc+sz,bc-sz+1:bc+sz))==2)
-            res = res + bc;
-        end
-    end
-    for br = brow
-        sz = min(size(equalRow,1)-br,br);
-        if all(sum(equalRow(br-sz+1:br+sz,br-sz+1:br+sz))==2)
-            res = res + 100 * br;
+    bdir = {bcol, brow};
+    tdir = {equalCol, equalRow};    
+    for j=1:2
+        for bd = bdir{j}
+            sz = min(size(tdir{j},1) - bd, bd);
+            if all(sum(tdir{j}(bd-sz+1:bd+sz,bd-sz+1:bd+sz))>=2)
+                res = res + multis(j) * bd;
+            end
         end
     end
 end
