dcf85149714faa2503cf9f1267c44bb0f24e5592
Felician <schmalzahar@web.de>
2023-12-13 21:14:47 +0100

    something wrong

--- 2023/a13.m
+++ 2023/a13.m
@@ -1,8 +1,8 @@
 input = [""; readlines('a13.txt');""];
 tic
 spl = find(input == "");
-res = 0;
-res2 = 0;
+res = NaN(100,1);
+res2 = NaN(100,1);
 multis = [1 100]; 
 for i=2:length(spl)    
     block = char(input(spl(i-1)+1:spl(i)-1));
@@ -23,9 +23,9 @@ for i=2:length(spl)
             bd = bdir1{j}(k);
             sz = min(size(tdir1{j},1) - bd, bd);
             if all(diag(tdir{j}(bd-sz+1:bd+sz,bd-sz+1:bd+sz))) && all(diag(tdir{j}(bd-sz+1:bd+sz,bd+sz:-1:bd-sz+1)))
-                res = res + multis(j) * bd;            
+                res(i-1) = multis(j) * bd;            
             elseif all(diag(tdir1{j}(bd-sz+1:bd+sz,bd-sz+1:bd+sz))) && all(diag(tdir1{j}(bd-sz+1:bd+sz,bd+sz:-1:bd-sz+1)))
-                res2 = res2 + multis(j) * bd;
+                res2(i-1) = multis(j) * bd;
             end
         end
     end
