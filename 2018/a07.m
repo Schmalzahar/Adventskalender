ad01fe59a6c177de0a8cc0b1fef8da55e0396b65
Felician <schmalzahar@web.de>
2023-12-17 20:58:35 +0100

    2018 day 7 p2

--- 2018/a07.m
+++ 2018/a07.m
@@ -4,22 +4,42 @@ right_letters = char(input(:,4));
 letters = char((1:26)+64);
 start = unique(left_letters(~ismember(left_letters, right_letters)));
 conns = digraph(string(left_letters), string(right_letters));
-step_extra_dur = 0;
-T = table((0:20)', repelem('.',21,1), repelem('.',21,1), repelem(' ',21,1),'VariableNames',{'Second','Worker 1','Worker 2','Done'})
+step_extra_dur = 60;
+t_max = 1000;
+T = table((0:t_max)', repelem('.',t_max+1,1), repelem('.',t_max+1,1), repelem('.',t_max+1,1), repelem('.',t_max+1,1), repelem('.',t_max+1,1), repelem(' ',t_max+1,1),'VariableNames',{'Second','Worker 1','Worker 2','Worker 3','Worker 4','Worker 5','Done'});
 g = digraph;
-order = start(1);
+% order = start(1);
+order = '';
 tic
-order = getOrder(order, conns, T, step_extra_dur)
+available = start;
+running = '';
+for i=1:height(T)    
+    for j=1:5
+        if T(i,1+j).Variables == '.'
+            if i>1 && T(i-1,1+j).Variables ~= '.' 
+                running = running(T(i-1,1+j).Variables ~= running);
+                order = [order T(i-1,1+j).Variables];
+                available = getAvailable(order, conns);
+                available(ismember(available, running)) = [];
+            end
+            if ~isempty(available)
+                T(i:i+step_extra_dur+available(1)-65,j+1).Variables = repelem(available(1),available(1)-64+step_extra_dur,1);
+                running = [running available(1)];
+                available(1) = [];    
+            end
+        end
+    end
+end
+
+% order = getOrder(start(1), conns)
 toc
-function [order] = getOrder(order, conns, T, step_extra_dur)
+
+function available = getAvailable(order, conns)
     conn_edges = {};
     for j=1:length(order)
         [~,conn_edges{j,1}] = outedges(conns, order(j));
     end
-    conn_edges = cat(1,conn_edges{:});
-    cn_sorted = sort(conn_edges);
-    % only available if all other connecting nodes to the new node are
-    % already present
+    cn_sorted = sort(cat(1,conn_edges{:}));
     cn_sorted = cn_sorted(~ismember([cn_sorted{:}],order));
     opts = indegree(conns) == 0;
     nds = conns.Nodes.Variables;
@@ -28,15 +48,16 @@ function [order] = getOrder(order, conns, T, step_extra_dur)
     for i=1:length(cn_sorted)
         [~,in_nodes] = inedges(conns, cn_sorted{i});
         if all(ismember([in_nodes{:}], order))
-            possible = [possible, cn_sorted{i}];
-            break
+            possible = [possible, cn_sorted{i}];            
         end
     end
-    possible = sort(possible);
+    available = unique(possible);
+end
+
+function [order] = getOrder(order, conns)
+    possible = getAvailable(order, conns);
     if isempty(possible)
         return
     end
-    % new node - new possibilities
-    order = [order possible(1)];
-    [order] = getOrder(order, conns);
+    [order] = getOrder([order possible(1)], conns);
 end
