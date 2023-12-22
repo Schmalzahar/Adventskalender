763593ee14f23f2ad52c015100950918594d5ffc
Felician <schmalzahar@web.de>
2023-12-22 22:48:32 +0100

    2023, day 20, part 2

--- 2023/a20.m
+++ 2023/a20.m
@@ -20,23 +20,30 @@ end
 pulses = [0 0];
 newpulse = 0;
 new_in = "roadcaster";
-for k=1:1000000000000
+for k=1:15000
     while ~isempty(new_in)
         in = string(new_in(1));        
         new_in(1) = [];
         pulse = newpulse(1);
-        if strcmp(in,'rx') && pulse == 0
-            test = 1;
-        end
+        
         newpulse(1) = [];
         in_spl = strtrim(in.split(','));
         for i=1:height(in_spl)
             in = in_spl(i);
-            % if pulse == 1
-            %     pulses(2) = pulses(2) + 1;
-            % else
-            %     pulses(1) = pulses(1) + 1;
-            % end     
+            % tests for part 2
+            % if strcmp(in,'vm') && pulse == 0
+            %     test = 1; % k=4051,8102,12153
+            % end
+            % if strcmp(in,'vb') && pulse == 0
+            %     test = 1; % k=3793,7586,11379
+            % end
+            % if strcmp(in,'kv') && pulse == 0
+            %     test = 1; % k=4013,8026,12039
+            % end
+            % if strcmp(in,'kl') && pulse == 0
+            %     test = 1; % k=3917,7834,11751
+            % end
+ 
             if isKey(flip_vals, in) && ~pulse
                 if flip_vals(in) == 0
                     flip_vals(in) = 1;  
@@ -70,3 +77,32 @@ for k=1:1000000000000
 end
 prod(pulses)
 toc
+
+
+
+
+%% Part 2 build a graph
+gr = digraph();
+gr = addedge(gr,"roadcaster",[strtrim(map{'roadcaster'}{1}.split(','))]);
+t = in_keys(in_keys ~= 'broadmaster');
+for i=1:height(t)
+    gr = addedge(gr,t(i),strtrim(map{t(i)}{1}.split(',')));
+end
+gr = simplify(gr);
+h = plot(gr,'Layout','layered');
+highlight(h, conj_keys, 'NodeColor','red')
+highlight(h, 'll','rx','EdgeColor','magenta') % magenta: needs to be low
+highlight(h, ["vm", "vb", "kv", "kl"], "ll","EdgeColor","green") % green: needs to be high
+highlight(h, "th", "vm","EdgeColor","magenta")
+highlight(h, "tj", "vb","EdgeColor","magenta")
+highlight(h, "hb", "kv","EdgeColor","magenta")
+highlight(h, "ff", "kl","EdgeColor","magenta")
+% requirement for low rx: all high inputs into ll
+% requirement for high input into ll:
+% - low inputs into: vb, kv, vm, kl
+% - vb: every 3793 steps
+% - kv: every 4013 steps
+% - vm: every 4051 steps
+% - kl: every 3917 steps
+
+resp2 = lcm(lcm(lcm(4051,3793),4013),3917)
