54c40eaa7de5dfdfcb13ec0c70a4f6582e9998d1
Felician Putz <schmalzahar@web.de>
2024-01-06 13:43:51 +0100

    2018 day 17 part 1, 2

--- 2018/a17.m
+++ 2018/a17.m
@@ -1,7 +1,8 @@
 input = readlines("a17.txt");
-
+tic
 insp = input.split(', ');
-v = insp(contains(insp(:,1),'x'),:);
+ind = contains(insp(:,1),'x');
+v = insp(ind,:);
 r = []; c = [];
 for i=1:height(v)
     l = v(i,:);
@@ -11,7 +12,7 @@ for i=1:height(v)
     r = [r; y+1];
     c = [c; repelem(x,length(y),1)];
 end
-h = insp(contains(insp(:,1),'y'),:);
+h = insp(~ind,:);
 for j=1:height(h)
     l = h(j,:);
     xr = double(extract(l(2),digitsPattern))';
@@ -20,42 +21,37 @@ for j=1:height(h)
     c = [c; x];
     r = [r; repelem(y+1,length(x),1)];
 end
-ymin = 1; ymax = max(r)+1; xmin = min(c)-1; xmax = max(c)+1;
-
+ymin = min(r)-1; ymax = max(r)+1; xmin = min(c)-1; xmax = max(c)+1;
 map = zeros(ymax-ymin,xmax-xmin+1);
 inds = sub2ind(size(map),1+r-ymin,1+c-xmin);
 map(inds) = 1;
 map(1,500-xmin+1) = 2;
-
+toc
 %% let it flow
+tic
 % 2: source, 3: water encountered. 4: standing water
 pos_queue = [];
-start_pos = [1, 500-xmin+1];
-branch = false;
-while true
-    % create one square meter of water
-    % try to run down
-    if isempty(pos_queue)
-        pos = start_pos; % source
-    else
-        if size(pos_queue,1) == 1 && ~branch
-            start_pos = pos_queue;
-            pos_queue(1,:) = [];
-            pos = start_pos;
-        else
-            branch = true;
-            pos = pos_queue(1,:);
-            pos_queue(1,:) = [];
-        end
-    end
+pos = [1, 500-xmin+1];
+prev_starts = [];
+end_reached = 0;
+not_done = 1;
+while not_done
     while true
+        if pos(1) == height(map) || end_reached
+            if isempty(pos_queue)
+                % done
+                fprintf('Part 1 result = %d\n',sum(map == 3,'all') + sum(map == 4,'all'))
+                fprintf('Part 2 result = %d\n',sum(map == 4,'all'))
+                not_done = 0;
+                break
+            end            
+            while map(pos_queue(end,1),pos_queue(end,2)) ~= 3
+                pos_queue(end,:) = [];                
+            end
+            pos = pos_queue(end,:); pos_queue(end,:) = [];
+            end_reached = 0;
+        end
         pos(1) = pos(1) + 1;
-        if ~isempty(pos_queue) && ismember(pos,pos_queue,"rows")
-            test = 1;
-        end
-        if pos(1) > height(map)
-            break
-        end
         switch map(pos(1),pos(2))
             case 0
                 map(pos(1),pos(2)) = 3;
@@ -67,95 +63,51 @@ while true
                 r_hole = pos(2) + find(ismember(layer(2,(pos(2)+1):end),[0 3]),1,'first');
                 left_wall = find(layer(1,1:pos(2)-1) == 1,1,'last');
                 l_hole = find(ismember(layer(2,1:pos(2)),[0 3]),1,'last');
-                if right_wall < r_hole
+                if right_wall <= r_hole
                     r_hole = [];
-                end
-                if left_wall > l_hole
-                    l_hole = [];
-                end
-                if r_hole < right_wall
+                else
                     right_wall = [];
                 end
-                if l_hole > left_wall
+                if left_wall >= l_hole
+                    l_hole = [];
+                else
                     left_wall = [];
-                end
-                
-                % no hole and 2 walls: standing water
-                if isempty(l_hole) && isempty(r_hole) && ~isempty(left_wall) && ~isempty(right_wall)
+                end                
+                if isempty(l_hole) && isempty(r_hole)
                     % no hole, fill up with water
                     map(pos(1)-1,(left_wall+1):(right_wall-1)) = 4;
+                    pos = [pos(1)-2 pos(2)]; % dont add to prev_starts
                     break
-                end
-                % is there a hole in the bottom? If yes, no standing water,
-                % but instead flow there
+                end             
                 if isempty(left_wall) && isempty(right_wall)
-                    % will flow to either direction
+                    % will flow to both directions
                     map(pos(1)-1,l_hole:r_hole) = 3;
-                    pos_queue(end+1,:) = [pos(1)-1 l_hole];
-                    pos_queue(end+1,:) = [pos(1)-1 r_hole];
-                    break
+                    % continue on the left path and go down that path till
+                    % the end. Then, go to the right path                    
+                    pos_queue(end+1,:) = [pos(1)-1 r_hole];   
+                    pos_queue = unique(pos_queue,'rows');
+                    pos = [pos(1)-1 l_hole];                    
                 elseif isempty(right_wall) && isempty(l_hole)
                     % flows to the right
                     map(pos(1)-1,(left_wall+1):r_hole) = 3;
-                    new_pos = [pos(1)-1 r_hole];
-                    if ~isempty(pos_queue) && ismember(new_pos,pos_queue,"rows")
-                        branch = false;
-                    else
-                        pos_queue(end+1,:) = new_pos;
-                    end                    
-                    break
-                elseif isempty(left_wall) && isempty(r_hole)
-                    map(pos(1)-1,(l_hole):(right_wall-1)) = 3;
-                    new_pos = [pos(1)-1 l_hole];
-                    if ~isempty(pos_queue) && ismember(new_pos,pos_queue,"rows")
-                        branch = false;
-                    else
-                        pos_queue(end+1,:) = new_pos;
-                    end    
-                    break
+                    pos = [pos(1)-1 r_hole];                    
                 else
-                    ot = 1;
+                    % flows to the left
+                    map(pos(1)-1,(l_hole):(right_wall-1)) = 3;
+                    pos = [pos(1)-1 l_hole];
                 end
-                % if any(layer(2,(left_wall+1):(right_wall-1)) == 0) ...
-                %         || any(layer(2,(left_wall+1):(right_wall-1)) == 3)
-                % if any(ismember(layer(2,(left_wall+1):(right_wall-1)),[0 3]))
-                %     r_hole = pos(2) + find(ismember(layer(2,(pos(2)+1):(right_wall-1)),[0 3]),1,'first');
-                %     l_hole = find(ismember(layer(2,1:pos(2)),[0 3]),1,'last');
-                %     if l_hole < left_wall
-                %         l_hole = [];
-                %     end
-                %     if r_hole > right_wall
-                %         r_hole = [];
-                %     end
-                %     if isempty(l_hole) && ~isempty(r_hole)
-                %         % fill from the wall to r_hole
-                %         layer(1,(left_wall+1):r_hole) = 3;
-                %         map(pos(1)-1:pos(1),:) = layer;
-                %         % continue at the hole position
-                %         % pos = [pos(1)-1 r_hole];
-                %         pos_queue(end+1,:) = [pos(1)-1 r_hole];
-                %         break
-                %     else
-                %         todo = 1;
-                %     end
-                % 
-                % 
-                % 
-                % else
-                %     % no hole, fill up with water
-                %     layer(1,(left_wall+1):(right_wall-1)) = 4;
-                %     map(pos(1)-1:pos(1),:) = layer;
-                %     break
-                % end
-        end
-        
+                if ~isempty(prev_starts) && ismember(pos, prev_starts,"rows")
+                    end_reached = 1;
+                    break
+                end
+                prev_starts(end+1,:) = pos;
+                break
+        end        
     end
     imagesc(map)  
-    ylim([1 200])
-    xlim([120 290])
-    
+    % focus map around current pos
+    ylim([max(1,pos(1)-100) min(pos(1)+100,height(map))])
+    xlim([max(1,pos(2)-100) min(pos(2)+100,width(map))])    
     % axis equal
 end
-
-
-
+toc
