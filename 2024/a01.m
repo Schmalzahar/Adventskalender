90f0e6fe41797df0af1b1474ab8bbba1982438ee
Felician Putz <schmalzahar@web.de>
2024-12-01 13:54:37 +0100

    optimized

--- 2024/a01.m
+++ 2024/a01.m
@@ -1,19 +1,3 @@
-input = double(readlines("a01.txt").split(" "));
-tic
-input = [input(:,1) input(:,4)];
-s = sort(input);
-res = sum(abs(diff(s,1,2)))
-
-[h,a] = groupcounts(s(:,2));
-
-% t  = sum(s(:,2) == s(:,2)',2);
-
-score = 0;
-for i=1:size(s,1)
-    if any(s(i,1) == a)
-        score = score + h(s(i,1) == a) * s(i,1);
-    end
-    
-end
-score
-toc
+s = sort(readmatrix("a01.txt"));
+part_1 = sum(abs(diff(s,1,2)))
+part_2 = sum(s(:,1).*sum(s(:,1) == s(:,2)',2))
