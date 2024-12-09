34f33ea8bf083274ca1da63f490b5f8016d790d7
Felician <schmalzahar@web.de>
2024-12-09 23:22:44 +0100

    9 done

--- 2024/a09.m
+++ 2024/a09.m
@@ -1,19 +1,18 @@
 input = (char(readlines("a09.txt"))-'0');
 sys = [];
-% set file system
 i = 1;
 id = 0;
-id_free = -1;
 type = 0;
+space = {};
 while true
     in = input(i);
     if type == 0
         sys = [sys repelem(id,in)];
         id = id + 1;
     else
-        if in ~= 0
-            sys = [sys repelem(id_free,in)];
-            id_free = id_free - 1;
+        if in ~= 0        
+            space = editSpace(space, in, size(sys,2)+1);
+            sys = [sys repelem(-1,in)];
         end        
     end
     i = i+1;
@@ -42,28 +41,44 @@ part1 = uint64(sum(res .* sys_sort(1:idx)))
 %% part 2
 sys_sort = sys;
 group_id = sys(end);
+left = 0; right = 0;
 while true
     group_og_idx = find(sys_sort == group_id);
     group_len = numel(group_og_idx);
-    id_free = -1;
-    while true         
-        len_free_space = sum(sys_sort == id_free);
-        if len_free_space == 0
-            test = 1;
+    min_idx = [Inf 0]; % 1: index of new location, 2: size of new location
+    for i=group_len:size(space,2)
+        if ~isempty(space{i}) && min_idx(1) > space{i}(1) && group_og_idx(1) > space{i}(1)
+            min_idx(1) = space{i}(1); min_idx(2) = i;
         end
-        if len_free_space < group_len
-            id_free = id_free - 1;
-            if id_free < min(sys_sort)
-                break
-            end
-            continue
-        else            
-            idx = find(sys_sort == id_free,group_len);
-            sys_sort(idx) = group_id;
-            sys_sort(group_og_idx) = NaN;
-            break
-        end        
     end
+    if ~isinf(min_idx(1))
+        space{min_idx(2)}(1) = [];
+        % new gap at group_og_idx. Is there already a gap to the left or to
+        % the right? Either a gap size 1 which is 1 index to the left, or a
+        % gap sized 2 which is 2 indices to the left, e.g.
+        for g=1:size(space,2)
+            % left
+            if ~isempty(space{g}) && any(space{g} == group_og_idx(1)-g) && left == 0
+                left = g;
+                space{g}(space{g} == group_og_idx(1)-g) = [];
+            end
+            % right
+            if ~isempty(space{g}) && any(space{g} == group_og_idx(end)+g) && right == 0
+                right = g;
+                space{g}(space{g} == group_og_idx(end)+g) = [];
+            end
+        end
+
+        % extend in both directions, or only left
+        new_len = left + right + group_len;
+        space = editSpace(space, new_len, group_og_idx(1)-left);
+        left = 0; right = 0;
+        if group_len ~= min_idx(2)
+            space{min_idx(2)-group_len} = sort([min_idx(1)+group_len, space{min_idx(2)-group_len}]);
+        end
+        sys_sort(min_idx(1):min_idx(1)+group_len-1) = group_id;
+        sys_sort(group_og_idx) = -1;        
+    end     
     group_id = group_id - 1;
     if group_id == 0
         break
@@ -73,3 +88,11 @@ end
 res = 0:(numel(sys_sort)-1);
 sys_sort(sys_sort<0) = NaN;
 part2 = uint64(sum(res .* sys_sort,'omitnan'))
+
+function space = editSpace(space, new_len, idx)
+    if new_len <= size(space,2) && ~isempty(space{new_len})
+        space{new_len} = sort([space{new_len} idx]);
+    else
+        space{new_len} = idx;
+    end   
+end
