88252ad2dbc4dba47824127e2607180c08a8a4f0
Felician <schmalzahar@web.de>
2024-12-20 15:41:57 +0100

    20 no graph

--- 2024/a20.m
+++ 2024/a20.m
@@ -1,34 +1,27 @@
 input = char(readlines("a20.txt"));
+tic
 start_pos = find(input == 'S');
 fin = find(input == 'E');
-
-[s,t] = buildGraph(input);
-G = graph(s,t);
-P = shortestpath(G,start_pos,fin);
-origLen = numel(P)-1;
+[sorted, indices] = sortIndices(input, start_pos, fin);
+origLen = size(sorted,1) - 1;
 
 %% part 1/2
 tracker = configureDictionary("double","double");
 path = find(input ~= '#');
 [px, py] = ind2sub(size(input), path);
 maxDistance = 20;
-cache = configureDictionary("double","double");
+cache = configureDictionary("double","double"); % "find" is pretty slow
 for i=1:numel(path)
     distances = abs(px - px(i)) + abs(py - py(i));
     ind1 = distances <= maxDistance & distances > 1;
     inRange = [px(ind1) py(ind1) distances(ind1)];
 
     for k=1:size(inRange,1)
-        if isKey(cache, start_pos * 10000 + path(i))
-            P1_1 = cache(start_pos * 10000 + path(i));
-        else
-            P1_1 = numel(shortestpath(G,start_pos,path(i))) - 1;
-            cache(start_pos * 10000 + path(i)) = P1_1;
-        end
+        P1_1 = indices(i)-1;
         if isKey(cache, sub2ind(size(input),inRange(k,1),inRange(k,2)) * 10000 + fin)
             P1_2 = cache(sub2ind(size(input),inRange(k,1),inRange(k,2)) * 10000 + fin);
         else
-            P1_2 = numel(shortestpath(G,sub2ind(size(input),inRange(k,1),inRange(k,2)),fin)) - 1;
+            P1_2 = size(sorted,1) - find(all(sorted == inRange(k,1:2),2));
             cache(sub2ind(size(input),inRange(k,1),inRange(k,2)) * 10000 + fin) = P1_2;
         end
         
@@ -44,21 +37,31 @@ for i=1:numel(path)
 end
 
 sum(tracker.values)
+toc
 
-function [s,t] = buildGraph(grid)
-    s = []; t = [];
-    sz = size(grid,1);
+function [sorted, indices] = sortIndices(input, start, fin)
     dirs = [1 0; -1 0; 0 -1; 0 1];
-    for idx = 1:numel(grid)
-        if grid(idx) ~= '#'
-            [r,c] = ind2sub(size(grid),idx);
-            r = r + dirs(:,1); c = c + dirs(:,2);
-            valid = (r >= 1 & r <= sz & c >= 1 & c <= sz);
-            r = r(valid); c = c(valid);
-            idx_neigh = sub2ind(size(grid), r, c);
-            b = sum(grid(idx_neigh) ~= '#');
-            s(end+1:end+b) = idx;
-            t(end+1:end+b) = idx_neigh(grid(idx_neigh) ~= '#');
+    [px, py] = find(input ~= '#');
+    path = find(input ~= '#');
+    [rs,cs] = ind2sub(size(input),start);
+    sorted = zeros(size(px,1),2);
+    sorted(1,:) = [rs cs];
+    for i=2:size(px,1)-1
+        neigh = sorted(i-1,:) + dirs;
+        valid = all(neigh >= 1 & neigh <= size(input,1),2);
+        neigh = sub2ind(size(input),neigh(valid,1), neigh(valid,2));
+        neigh = neigh(input(neigh) ~= '#');
+        [a,b] = ind2sub(size(input), neigh);
+        if i>2
+            id1 = any([a,b] ~= sorted(i-2,:),2);
+        else
+            id1 = 1;
         end
-    end    
+        sorted(i,:) = [a(id1),b(id1)];
+    end
+    [rf,cf] = ind2sub(size(input),fin);
+    sorted(end,:) = [rf cf];
+
+    sorted_ind = sub2ind(size(input),sorted(:,1),sorted(:,2));
+    [~,indices] = ismember(path, sorted_ind);
 end
