8baafd06a3b26cec18edeeb8e6c7011f1c178949
Felician Putz <schmalzahar@web.de>
2024-12-23 01:16:55 +0100

    21 done

--- 2024/a21.m
+++ 2024/a21.m
@@ -1,115 +1,125 @@
 input = readlines("a21.txt");
-% input = "37"
 
-keypad = ['7' '8' '9';'4' '5' '6'; '1' '2' '3'; NaN '0' 'A']; % 10: A
+tic
+keypad = ['789';
+          '456';
+          '123';
+          ' 0A'];
 
-robotPad = [NaN '^' 'A'; '<' 'v' '>'];
+robotPad = [' ^A';
+            '<v>'];
+
+maxLevel = 4-2+25;
+
+numMoves = movesX(keypad);
+padMoves = movesX(robotPad);
+
 compl = 0;
-for i=1:height(input)
-    door_code = char(input(i));
-
-    %% moves on the door code
-    % current position: A
-    [cpx, cpy] = find('A' == keypad);
-    instrRob1 = '';
-    for j=1:length(door_code)
-        door_let = door_code(j);
-        [x, y] = find(door_let == keypad);
-    
-        % moves to do:
-        moveUp = cpx - x;
-        moveLeft = cpy - y;
-
-        newInstrRob1 = '';
-
-        if moveLeft > 0
-            newInstrRob1 = [newInstrRob1  repelem('<',moveLeft)];
-        else
-            newInstrRob1 = [newInstrRob1  repelem('>',-moveLeft)];
-        end
-
-        if moveUp > 0
-            newInstrRob1 = [newInstrRob1  repelem('^',moveUp)];
-        else
-            newInstrRob1 = [newInstrRob1  repelem('v',-moveUp)];
-        end
-
-        if moveUp ~= 0 && moveLeft ~= 0
-            % more than one optimal move possible.
-            combs = unique(perms(newInstrRob1),'rows');
-            % also make sure we dont walk over the empty field
-            if cpx == 4
-                % in this case we cannot have two <'s before moving up
-                left_idx = combs == '<'; up_idx = combs == '^';
-                [~,firstUp] = max(up_idx, [], 2);
-                mask = (1:size(combs, 2)) < firstUp;
-                leftBeforeUp = sum(combs == '<' & mask, 2) < 2;
-                combs = combs(leftBeforeUp,:);     
-
-                
-            elseif x == 4 && cpy == 1
-                % in this case we cannot move down to x when we are at y=1
-                test = 1;
-            end
-            if ~isempty(instrRob1)
-            % instrRob1 = [repmat(instrRob1,size(combs,1),1) combs repmat('A',size(combs,1),1)];
-                instrRob1 = [repmat(instrRob1,size(combs,1),1) repmat(combs,size(instrRob1,1),1)];
-                instrRob1 = [instrRob1 repmat('A',size(instrRob1,1),1)];
-            else
-                instrRob1 = [combs repelem('A',size(combs,1),1)];
-            end
-        else
-            if ~isempty(instrRob1)
-                instrRob1 = [instrRob1 repmat([newInstrRob1 'A'],size(instrRob1,1),1) ];
-            else
-                instrRob1 = [newInstrRob1 'A'];
-            end
-        end        
-        cpx = x; cpy = y;
+for in = input'
+    seq = ['A' char(in)]; % we always start at A
+    shortestSeq = 0;
+    for k=1:numel(seq)-1
+        out = minNumPresses(seq(k), seq(k+1), numMoves, padMoves, 1, maxLevel);
+        shortestSeq = out + shortestSeq;
     end
-
-    %% Now we have the first code.
-    inputCode = string(instrRob1);    
-    for j=1:25
-        [cpx, cpy] = find('A' == robotPad);
-        new_inst = [];
-        for p=1:size(inputCode,1)            
-            instrNextRob = '';
-            ipC = char(inputCode(p));
-            for k=1:length(ipC)
-                rob_let = ipC(k);
-                [x, y] = find(rob_let == robotPad);
-    
-                % moves to do:
-                moveUp = cpx - x;
-                moveLeft = cpy - y;
-    
-                if moveUp < 0
-                    instrNextRob = [instrNextRob  repelem('v',-moveUp)];
-                end
-    
-                if moveLeft < 0
-                    instrNextRob = [instrNextRob  repelem('>',-moveLeft)];
-                elseif moveLeft > 0
-                    instrNextRob = [instrNextRob  repelem('<',moveLeft)];
-                end
-    
-                if moveUp > 0
-                    instrNextRob = [instrNextRob  repelem('^',moveUp)];
-                end
-    
-                % Press A
-                instrNextRob = [instrNextRob 'A'];
-                cpx = x; cpy = y;
-            end    
-            % pass this on to the next robot
-            % inputCode = instrNextRob;
-            new_inst = [new_inst; string(instrNextRob)];
-            [cpx, cpy] = find('A' == robotPad);
-        end
-        inputCode = new_inst;
-    end
-
-    compl = compl + min(strlength(inputCode)) * str2double(door_code(1:3));
+    compl = compl + shortestSeq * str2double(seq(2:end-1));
 end
-compl
+int64(compl)
+toc
+
+function minLength = minNumPresses(start, fin, X1, X2, level, maxLevel)    
+
+    if level == 1
+        padStruct = X1;
+    else
+        padStruct = X2;
+    end
+
+    if level == maxLevel
+        minLength = 1;
+        return
+    end   
+
+    persistent lenCache
+    if isempty(lenCache)
+        lenCache = NaN(12, 12, maxLevel-1);
+    end
+
+    ops = padStruct.ops; pad = padStruct.pad;
+
+    %
+    startPos = start == pad;
+    finPos = fin == pad;
+
+    cacheVal = lenCache(startPos, finPos, level);
+    if ~isnan(cacheVal)
+        minLength = cacheVal;
+        return
+    end
+
+    seqs = ops{startPos, finPos};
+    lengths = zeros(size(seqs,1),1);
+    for o=1:size(seqs,1)
+        seq = seqs{o,1};
+        for s=1:numel(seq)-1
+            lengths(o) = lengths(o) + minNumPresses(seq(s), seq(s+1), X1, X2, level + 1, maxLevel);
+        end      
+    end
+    minLength = min(lengths);
+    lenCache(startPos, finPos, level) = minLength;
+end
+
+
+
+function X = movesX(pad)
+    % returns the possible moves from each point for both keypad variants.
+    numFields = numel(pad);
+    [r,c] = ind2sub(size(pad), (1:numFields)');
+    moves = cell(numFields, numFields);
+    dr = r' - r;
+    dc = c' - c;
+    sgn = ['^ v';'< >'];
+
+    for i=1:numFields
+        for j=1:numFields
+            if i == j
+                moves{i,j} = {'AA'};
+                continue
+            end
+            % possible moves to move from index i to j
+            % moves to do:
+            moveDown = dr(i,j);
+            sD = sign(moveDown);          
+            moveRight = dc(i,j); 
+            sR = sign(moveRight);
+            upDown = repelem(sgn(1,sD+2), abs(moveDown));
+            leftRight = repelem(sgn(2,sR+2), abs(moveRight));            
+
+            % now remove options that move over empty field.
+            % path of upDown and leftRight
+            pUD = @(x) [(r(i)+sD:sD:r(j))' repelem(c(x),abs(moveDown),1)];
+            pLR = @(x) [repelem(r(x),abs(moveRight),1) (c(i)+sR:sR:c(j))'];
+            % paths option A and B
+            pA = [pUD(i); pLR(j)]; 
+            pB = [pLR(i); pUD(j)];
+            padPathA = pad(sub2ind(size(pad),pA(:,1),pA(:,2)));
+            padPathB = pad(sub2ind(size(pad),pB(:,1),pB(:,2)));           
+
+            % option A: move up/down first; option B: move left/right first
+            % first A accounts for robot always starting at A
+            % last A: robot has to press A
+            temp = cell(0,1);
+            if ~any(padPathA == ' ')
+                temp{end+1,1} = ['A' upDown leftRight 'A'];
+            end
+            if ~any(padPathB == ' ') && ~(moveDown == 0 || moveRight == 0)
+                temp{end+1,1} = ['A' leftRight upDown 'A']; 
+            end
+            moves{i,j} = temp;
+        end
+    end
+
+    X = struct;
+	X.ops = moves;
+	X.pad = pad;    
+end
